4794a781ee0ac653c45b28b59a464c8c3e0d5943
debnathk <debnathk1997@gmail.com>
2025-12-19 11:58:24 -0500

    GramSeq: DataLoader working

--- gramseq-main/run_gramseq.sh
+++ gramseq-main/run_gramseq.sh
@@ -6,18 +6,17 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=150G
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --partition=cpu
 #SBATCH --time=30-00:00:00
 
 echo "Experiment starts at: $(date)"
 
 eval "$(conda shell.bash hook)"
-conda activate aidd-tensorflow
+conda activate aidd-pytorch
 # conda activate py10-tf210
 echo "Using python from $(which python)"
 
-module load cuda/12.3
+# module load cuda/12.3
 
 # Print cuda-core info
 # python -c "import tensorflow as tf; print(f'Device(s): {tf.config.list_physical_devices()}')"
